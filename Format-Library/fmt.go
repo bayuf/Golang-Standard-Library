dadacfabd3d29fccc3f84353870ec0bf0d2fa6cb
Bayu Firmansyah <bayufirmansyah203@gmail.com>
2025-10-30 13:19:07 +0700

    Add fmt.go

---
+++ Format-Library/fmt.go
@@ -0,0 +1,16 @@
+package main
+
+// https://pkg.go.dev/fmt
+
+import "fmt"
+
+func main() {
+	name := "Bayu Firmansyah"
+	age := 25
+
+	// Pakai format Println
+	fmt.Println("Halo nama saya", name, "Umur saya", age, "Tahun")
+
+	// Pakai format Printf
+	fmt.Printf("Halo nama saya %s, umur saya %d Tahun", name, age)
+}
